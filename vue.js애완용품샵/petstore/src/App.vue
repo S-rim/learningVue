d8638b3907fc79c51b99be2b5beba90547d0898e
Srim <deamoon0801@naver.com>
2020-03-10 03:32:31 +0900

    화면전환 시 fadein 구현

--- vue.js애완용품샵/petstore/src/App.vue
+++ vue.js애완용품샵/petstore/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <transition name="fade" mode="out-in">
     <router-view/>
+    </transition>>
   </div>
 </template>
 
@@ -9,3 +11,13 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition : opacity .5s ease-out;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity : 0;
+}
+</style>
