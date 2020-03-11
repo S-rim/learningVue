79b7e10256c4a451d09e78eb6fe4d239ee4a9994
Srim <deamoon0801@naver.com>
2020-03-12 01:27:22 +0900

    전역 커스텀 지시자 사용 방법

---
+++ JS/전역 커스텀 지시자.js
@@ -0,0 +1,26 @@
+Vue.directive('style-me', {
+    bind(el, binding) {
+        el.style.color = binding.value || "blue";
+
+        if (binding.modifiers.large)
+        {
+            el.style.fontSize = "42px";
+        }
+
+        else if (binding.modifiers.small)
+        {
+            el.style.fontSize = "17px";
+        }
+
+        el.className = binding.arg;
+    }
+});
+
+new Vue ({
+   el : '#app',
+   data () {
+       return {
+           Welcome : '안녕!'
+       }
+   } 
+});
