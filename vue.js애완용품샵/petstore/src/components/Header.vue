adb061b6d008cf8f2340a71fe103e41b2bfceeb5
Srim <deamoon0801@naver.com>
2020-03-15 17:12:48 +0900

    router-link를 사용함으로써 쓰지 않게 된 메소드 코드 삭제

--- vue.js애완용품샵/petstore/src/components/Header.vue
+++ vue.js애완용품샵/petstore/src/components/Header.vue
@@ -6,7 +6,7 @@
                     <h1></h1>
                 </div>
                 <div class="nav navbar-nav navbar-right cart">
-                        <router-link active-class="active" tag="button" class="btn btn-default btn-lg" :to="{name: 'Form'}">
+                        <router-link active-class="active" class="btn btn-default btn-lg" :to="{name: 'Form'}">
                         <span class="glyphicon glyphicon-shopping-cart">{{cartItemCount}}</span> 체크아웃 
                         </router-link>
                 </div>
@@ -24,11 +24,7 @@ export default {
         }
     },
     props : ['cartItemCount'],
-    methods : {
-        showCheckOut() {
-            this.$router.push({name: 'From'});
-        }
-    }
+
 }
 </script>
 
