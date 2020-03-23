541baeacbd2a7b5df9e7a3b422ff7b230fd38faa
Srim <deamoon0801@naver.com>
2020-03-24 00:14:11 +0900

    Main 컴포넌트 firebase 추가

--- vue.js애완용품샵/petstore/src/components/Main.vue
+++ vue.js애완용품샵/petstore/src/components/Main.vue
@@ -51,9 +51,13 @@
 <script>
 import MyHeader from './Header.vue';
 import {mapGetters} from 'vuex';
+import { productsRef } from '../firebase';
 export default {
   name: 'imain',
-  data() {
+  firebase: {
+      products: productsRef
+  },
+  data () {
     return {
       cart: []
     }
@@ -64,14 +68,15 @@ export default {
       return myProduct.rating - n >= 0;
     },
     addToCart(aProduct) {
-      this.cart.push(aProduct.id);
+      this.cart.push( aProduct.id );
     },
     canAddToCart(aProduct) {
+      //return this.product.availableInventory > this.cartItemCount;
       return aProduct.availableInventory > this.cartCount(aProduct.id);
     },
     cartCount(id) {
       let count = 0;
-      for (let i = 0; i < this.cart.length; i++) {
+      for(var i = 0; i < this.cart.length; i++) {
         if (this.cart[i] === id) {
           count++;
         }
@@ -80,48 +85,46 @@ export default {
     }
   },
   computed: {
+    ...mapGetters([
+          'session'
+    ]),
     cartItemCount() {
       return this.cart.length || '';
     },
     sortedProducts() {
-      if (this.products.length > 0) {
+      if(this.products.length > 0) {
         let productsArray = this.products.slice(0);
         function compare(a, b) {
-          if (a.title.toLowerCase() < b.title.toLowerCase())
-            return -1;
-          if (a.title.toLowerCase() > b.title.toLowerCase())
-            return 1;
+          if(a.title.toLowerCase() < b.title.toLowerCase())
+          return -1;
+          if(a.title.toLowerCase() > b.title.toLowerCase())
+          return 1;
           return 0;
         }
         return productsArray.sort(compare);
       }
-    },
-    ...mapGetters([
-      'products'
-    ])
+    }
   },
   filters: {
     formatPrice(price) {
-      if (!parseInt(price)) {
-        return '';
-      }
+      if (!parseInt(price)) { return ""; }
       if (price > 99999) {
         var priceString = (price / 100).toFixed(2);
         var priceArray = priceString.split("").reverse();
         var index = 3;
         while (priceArray.length > index + 3) {
-          priceArray.splice(index+3, 0, ',');
+          priceArray.splice(index+3, 0, ",");
           index += 4;
         }
-        return '$' + priceArray.reverse().join('');
+        return "$" + priceArray.reverse().join("");
       } else {
-        return '$' + (price / 100).toFixed(2);
+        return "$" + (price / 100).toFixed(2);
       }
     }
   },
   created: function() {
     this.$store.dispatch('initStore');
-    }
+  }
 }
 </script>
 
