e79552d574096342c727fc4d4a65f2356d2cfbbd
Srim <deamoon0801@naver.com>
2020-04-02 23:41:13 +0900

    firebase app

--- vue.js애완용품샵/petstore/src/main.js
+++ vue.js애완용품샵/petstore/src/main.js
@@ -7,10 +7,22 @@ require('./assets/app.css')
 import { store } from './store/store';
 import firebase from 'firebase';
 import './firebase';
-import VueFire from 'vuefire';
 
-Vue.use(VueFire);
+
 Vue.config.productionTip = false
+
+let firebaseConfig = {
+  apiKey: "AIzaSyA7YvrpHZqSoAyxGNuGJNRK4bvsatWkKo0",
+  authDomain: "petstore-8fc21.firebaseapp.com",
+  databaseURL: "https://petstore-8fc21.firebaseio.com",
+  projectId: "petstore-8fc21",
+  storageBucket: "petstore-8fc21.appspot.com",
+  messagingSenderId: "872420165769",
+  appId: "1:872420165769:web:0c7ac26a3c22b0b6215e58",
+  measurementId: "G-G2YQT3J0FT"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 /* eslint-disable no-new */
 new Vue({
